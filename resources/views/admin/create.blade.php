1ca91db5f297b9f3e1c48023cc1642e5dd9dc6be
Akishino1998 <ekopujianto48@gmail.com>
2019-02-01 03:23:31 +0800

    v1

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,250 +1,140 @@
 @extends('admin.layouts.master')
 @section('konten')
 	<section role="main" class="content-body">
-					<header class="page-header">
-						<h2>Tambah Data</h2>
-
-						<div class="right-wrapper pull-right">
-							<ol class="breadcrumbs">
-								<li>
-									<a href="/index">
-										<i class="fa fa-home"></i>
-									</a>
-								</li>
-								<li><span>Form Pengisian data</span></li>
-								<li><span>Tahap Awal</span></li>
-							</ol>
-
-							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
-						</div>
-					</header>
-
-					<!-- start: page -->
-						<div class="row">
-							<div class="col-lg-12">
-								<section class="panel">
-									<header class="panel-heading">
-										<div class="panel-actions">
-											<a href="#" class="fa fa-caret-down"></a>
-											<a href="#" class="fa fa-times"></a>
-										</div>
-
-										<h2 class="panel-title">ISI DATA DIRI DENGAN BENAR</h2>
-									</header>
-									<div class="panel-body">
-										<form class="form-horizontal form-bordered" method="get">
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Nama Depan</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Nama Belakang</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Nomor Plat	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="no_polisi">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Alamat	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="alamat">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Merk Type</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="merktype">
-													<span class="help-block">Contoh "honda,yamaha, dll".</span>
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Jenis Model	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="jenis_model">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Isi Silinder	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="isi_silinder">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Warna Kb	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="warna_kb">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">No Rangka	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="no_rangka">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">No Mesin</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="no_mesin">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">No BPKB	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="no_bpkb">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Tanggal Berlaku	</label>
-												<div class="col-md-6">
-													<input type="date" class="form-control" id="inputDefault" name="berlaku_sampai">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Warna TNKB	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="warna_tnkb">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label" for="inputDefault">Bahan Bakar	</label>
-												<div class="col-md-6">
-													<input type="text" class="form-control" id="inputDefault" name="bahan_bakar">
-												</div>
-											</div>
-
-											<div class="form-group">
-												<label class="col-md-3 control-label">Foto</label>
-												<div class="col-md-6">
-													<div class="fileupload fileupload-new" data-provides="fileupload">
-														<div class="input-append">
-															<div class="uneditable-input">
-																<i class="fa fa-file fileupload-exists"></i>
-																<span class="fileupload-preview"></span>
-															</div>
-															<span class="btn btn-default btn-file">
-																<span class="fileupload-exists">Change</span>
-																<span class="fileupload-new">Select file</span>
-																<input type="file" name="foto" />
-															</span>
-															<a href="#" class="btn btn-default fileupload-exists" data-dismiss="fileupload">Remove</a>
-															<span class="help-block">*Foto motor Harus sesuai dengan warna STNK.</span>
-														</div>
-													</div>
-												</div>
-											</div>
-
-
-											<div class="form-group">
-												<label class="control-label col-md-3">Akun Pribadi</label>
-												<div class="col-md-6">
-													<section class="form-group-vertical">
-														<div class="input-group input-group-icon">
-															<span class="input-group-addon">
-																<span class="icon"><i class="fa fa-user"></i></span>
-															</span>
-															<input class="form-control" type="text" placeholder="Username">
-														</div>
-														<div class="input-group input-group-icon">
-															<span class="input-group-addon">
-																<span class="icon"><i class="fa fa-key"></i></span>
-															</span>
-															<input class="form-control" type="password" placeholder="Password">
-														</div>
-													</section>
-
-
-
-					<!-- end: page -->
-				</section>
-			</div>
-
-			<aside id="sidebar-right" class="sidebar-right">
-				<div class="nano">
-					<div class="nano-content">
-						<a href="#" class="mobile-close visible-xs">
-							Collapse <i class="fa fa-chevron-right"></i>
+		<header class="page-header">
+			<h2>Tambah Data</h2>
+			<div class="right-wrapper pull-right">
+				<ol class="breadcrumbs">
+					<li>
+						<a href="/index">
+							<i class="fa fa-home"></i>
 						</a>
-
-						<div class="sidebar-right-wrapper">
-
-							<div class="sidebar-widget widget-calendar">
-								<h6>Upcoming Tasks</h6>
-								<div data-plugin-datepicker data-plugin-skin="dark" ></div>
-
-								<ul>
-									<li>
-										<time datetime="2014-04-19T00:00+00:00">04/19/2014</time>
-										<span>Company Meeting</span>
-									</li>
-								</ul>
-							</div>
-
-							<div class="sidebar-widget widget-friends">
-								<h6>Friends</h6>
-								<ul>
-									<li class="status-online">
-										<figure class="profile-picture">
-											<img src="assets/images/!sample-user.jpg" alt="Joseph Doe" class="img-circle">
-										</figure>
-										<div class="profile-info">
-											<span class="name">Joseph Doe Junior</span>
-											<span class="title">Hey, how are you?</span>
-										</div>
-									</li>
-									<li class="status-online">
-										<figure class="profile-picture">
-											<img src="assets/images/!sample-user.jpg" alt="Joseph Doe" class="img-circle">
-										</figure>
-										<div class="profile-info">
-											<span class="name">Joseph Doe Junior</span>
-											<span class="title">Hey, how are you?</span>
-										</div>
-									</li>
-									<li class="status-offline">
-										<figure class="profile-picture">
-											<img src="assets/images/!sample-user.jpg" alt="Joseph Doe" class="img-circle">
-										</figure>
-										<div class="profile-info">
-											<span class="name">Joseph Doe Junior</span>
-											<span class="title">Hey, how are you?</span>
-										</div>
-									</li>
-									<li class="status-offline">
-										<figure class="profile-picture">
-											<img src="assets/images/!sample-user.jpg" alt="Joseph Doe" class="img-circle">
-										</figure>
-										<div class="profile-info">
-											<span class="name">Joseph Doe Junior</span>
-											<span class="title">Hey, how are you?</span>
-										</div>
-									</li>
-								</ul>
-							</div>
-
+					</li>
+					<li><span>Form Pengisian data</span></li>
+					<li><span>Tahap Awal</span></li>
+				</ol>
+				<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+			</div>
+		</header>
+		<div class="row">
+			<div class="col-lg-12">
+				<section class="panel">
+					<header class="panel-heading">
+						<div class="panel-actions">
+							<a href="#" class="fa fa-caret-down"></a>
+							<a href="#" class="fa fa-times"></a>
 						</div>
+						<h2 class="panel-title">ISI DATA DIRI DENGAN BENAR</h2>
+					</header>
+					<div class="panel-body">
+						<form class="form-horizontal form-bordered" method="post">
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">No. STNK</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="no">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Nama Lengkap</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="nama_pemilik">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Nomor Plat	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="no_polisi">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Alamat	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="alamat">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Merk Type</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="merktype">
+									<span class="help-block">Contoh "honda,yamaha, dll".</span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Jenis Model	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="jenis_model">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Isi Silinder	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="isi_silinder">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Warna Kb	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="warna_kb">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">No Rangka	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="no_rangka">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">No Mesin</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="no_mesin">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">No BPKB	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="no_bpkb">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Tanggal Berlaku	</label>
+								<div class="col-md-6">
+									<input type="date" class="form-control" id="inputDefault" name="berlaku_sampai">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Warna TNKB	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="warna_tnkb">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="inputDefault">Bahan Bakar	</label>
+								<div class="col-md-6">
+									<input type="text" class="form-control" id="inputDefault" name="bahan_bakar">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Foto</label>
+								<div class="col-md-6">
+									<div class="fileupload fileupload-new" data-provides="fileupload">
+										<div class="input-append">
+											<div class="uneditable-input">
+												<span class="fileupload-preview"></span>
+											</div>
+											<span class="btn btn-default btn-file">
+												<span class="fileupload-new">Select file</span>
+												<input type="file" name="foto" />
+											</span>
+											<a href="#" class="btn btn-default fileupload-exists" data-dismiss="fileupload">Remove</a>
+											<span class="help-block">*Foto motor Harus sesuai dengan warna STNK.</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</form>
 					</div>
-				</div>
-			</aside>
-		</section>
+			</section>
+		</div>
+	</section>
 
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,48 +1,44 @@
 @extends('admin.layouts.master')
 @section('konten')
-	
-
-				<section role="main" class="content-body">
-					<header class="page-header">
-						<h2>Dashboard</h2>
-
-						<div class="right-wrapper pull-right">
-							<ol class="breadcrumbs">
-								<li>
-									<a href="index.html">
-										<i class="fa fa-home"></i>
-									</a>
-								</li>
-								<li><span>Dashboard</span></li>
-							</ol>
-
-							<a class="sidebar-right-toggle" ><i class="fa fa-chevron-left"></i></a>
+	<section role="main" class="content-body">
+		<header class="page-header">
+			<h2>Dashboard</h2>
+			<div class="right-wrapper pull-right">
+				<ol class="breadcrumbs">
+					<li>
+						<a href="index.html">
+							<i class="fa fa-home"></i>
+						</a>
+					</li>
+					<li><span>Dashboard</span></li>
+				</ol>
+				<a class="sidebar-right-toggle" ><i class="fa fa-chevron-left"></i></a>
+			</div>
+		</header>
+		<div class="col-md-6 col-lg-12 col-xl-6">
+			<div class="row">
+				<div class="col-md-12 col-lg-6 col-xl-6">
+					<section class="panel panel-featured-left panel-featured-primary">
+						<div class="panel-body">
+							<div class="widget-summary">
+								<div class="widget-summary-col widget-summary-col-icon">
+									<div class="summary-icon bg-primary">
+										<i class="fa fa-life-ring"></i>
+									</div>
+								</div>
+								<div class="widget-summary-col">
+									<div class="summary">
+										<h4 class="title">Jumlah Kendaraan</h4>
+										<div class="info">
+										<strong class="amount">9999999</strong>
+										<!-- <span class="text-primary">(14 unread)</span> -->
+									</div>
+								</div>
+								<div class="summary-footer">
+									<a class="text-muted text-uppercase">(view all)</a>
+								</div>
+							</div>
 						</div>
-					</header>
-					<div class="col-md-6 col-lg-12 col-xl-6">
-						<div class="row">
-							<div class="col-md-12 col-lg-6 col-xl-6">
-								<section class="panel panel-featured-left panel-featured-primary">
-									<div class="panel-body">
-										<div class="widget-summary">
-											<div class="widget-summary-col widget-summary-col-icon">
-												<div class="summary-icon bg-primary">
-													<i class="fa fa-life-ring"></i>
-												</div>
-											</div>
-											<div class="widget-summary-col">
-												<div class="summary">
-													<h4 class="title">Jumlah Kendaraan</h4>
-													<div class="info">
-														<strong class="amount">9999999</strong>
-														<!-- <span class="text-primary">(14 unread)</span> -->
-													</div>
-												</div>
-												<div class="summary-footer">
-													<a class="text-muted text-uppercase">(view all)</a>
-												</div>
-											</div>
-										</div>
 									</div>
 								</section>
 							</div>
